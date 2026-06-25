347b4bd4f5fbca12199716bd62d792226d02e9a5
Francisco Godoy <fra.godoyc@duocuc.cl>
2026-06-25 09:31:10 -0400

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -10,7 +10,6 @@ const API_BASE = "http://tienda-backend:3001/api/productos";
 // Ejemplo: const API_BASE = "http://10.0.2.30:3001/api/productos";
 
 
-
 let editandoId = null;
 
 const tbody = document.getElementById("tbodyProductos");
